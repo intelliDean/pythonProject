4ae6f743fba0048af7265db06c555f676e2df677
Michael Dean <o.michaeldean@gmail.com>
2022-12-11 01:21:07 +0100

    new update

--- Dean/Assignment/chapter2/divisible_by_3.py
+++ Dean/Assignment/chapter2/divisible_by_3.py
@@ -3,5 +3,6 @@ def divisible_by_3(number):
 
 
 if __name__ == '__main__':
-    number = int(input("enter the number\n"))
-    print(divisible_by_3(number))
+    num = divisible_by_3(45)
+    print();
+
